85ce2c9cb1a41dbc6b00c0eb92ff3d6fc5fda79e
Admin <vanpho02@gmail.com>
2017-03-14 15:20:07 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
